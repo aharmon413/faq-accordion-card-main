f5090b5a1a57dd84a79a91ed7a56339d89092734
Amber <aharmon413@gmail.com>
2021-10-04 15:49:19 -0400

    Design for mobile devices complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('js is working!');
+const accordions = document.getElementsByClassName('accordion-title');
+
+function openAccordion(icon, panel) {
+    icon.style.transform = 'rotate(180deg)';
+    panel.style.display = 'block';
+    panel.style.maxHeight = panel.scrollHeight + "px";
+}
+
+function closeAccordion(icon, panel) {
+    icon.style.transform = null;
+    panel.style.display = 'none';
+    panel.style.maxHeight = null;
+}
+
+function hideAll() {
+    for (let accordion of accordions) {
+        accordion.classList.remove('active');
+        closeAccordion(accordion.querySelector('img'), accordion.nextElementSibling);
+    }
+}
+
+for (let accordion of accordions) {
+    accordion.addEventListener("click", function() {
+        let icon = accordion.querySelector('img');
+        let panel = accordion.nextElementSibling;
+        if (panel.style.maxHeight) {
+            closeAccordion(icon, panel);
+            accordion.setAttribute('aria-expanded', 'false');
+        } else {
+            hideAll();
+            openAccordion(icon, panel);
+            accordion.setAttribute('aria-expanded', 'true');
+        }
+        accordion.classList.toggle('active');
+    });
+}
